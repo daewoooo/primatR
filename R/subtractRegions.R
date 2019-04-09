ed4346b22d3e06be694db24ab8d0e6d8cb7941da
daewoooo <porubsky@eee-102737.(none)>
2019-04-09 11:35:23 -0700

    Added function to subtract regions overlapping with a set of genomic ranges

--- R/subtractRegions.R
+++ R/subtractRegions.R
@@ -2,11 +2,12 @@
 #'
 #' @param gr A \code{\link{GRanges-class}} object of ranges from which remove.gr are removed.
 #' @param remove.gr A \code{\link{GRanges-class}} object of ranges to subtract from gr.
+#' @param mode Select 'flanks' to subtract only flanking regions or 'all' to subtract all regions what creates disjoined ranges.
 #' @return A \code{\link{GRanges-class}} object.
 #' @author David Porubsky
 #' @export
 #' 
-subtractFlankingRegions <- function(gr = NULL, remove.gr = NULL) {
+subtractRegions <- function(gr = NULL, remove.gr = NULL, mode = 'flanks') {
   ## Get set of non-overlapping ranges between gr of interest and mask - remove.gr
   disjoin.gr <- disjoin(c(gr[,0], remove.gr[,0]))
   ## Get only non-overlapping ranges that overlaps with gr of interest
@@ -18,7 +19,10 @@ subtractFlankingRegions <- function(gr = NULL, remove.gr = NULL) {
   ## Merge splitted unique ranges that belong to the same gr of interest
   unique.gr.collapse <- unique.gr[subjectHits(hits)]
   unique.gr.collapse$ID <- queryHits(hits)
-  unique.merged.gr <- collapseBins(unique.gr.collapse, id.field = 1)
-  
-  return(unique.merged.gr)
+  if (mode == 'flanks') {
+    unique.merged.gr <- collapseBins(unique.gr.collapse, id.field = 1)
+    return(unique.merged.gr)
+  } else {
+    return(unique.gr.collapse)
+  } 
 }
