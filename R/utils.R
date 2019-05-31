d3b691125c14f713d7de1cf0bfe11556e5fb9381
daewoooo <porubsky@eee-102737.(none)>
2019-05-30 17:30:00 -0700

    Small code change

--- R/utils.R
+++ R/utils.R
@@ -165,16 +165,29 @@ countUniqueReadIDs <- function(readIDs) {
 #' 
 #' @param gr A \code{\link{GRanges}} object.
 #' @param id.field A field column used to collapse ranges with the same value.
-#' 
+#' @param measure.field A field column that contains measured value to be sum up.
 #' @author David Porubsky
 #' @export
 #' 
-collapseBins <- function(gr, id.field=0) {
-  ind.last <- cumsum(runLength(Rle(mcols(gr)[,id.field]))) ##get indices of last range in a consecutive(RLE) run of the same value
-  ind.first <- c(1,cumsum(runLength(Rle(mcols(gr)[,id.field]))) + 1) ##get indices of first range in a consecutive(RLE) run of the same value
-  ind.first <- ind.first[-length(ind.first)]  ##erase last index from first range indices
+collapseBins <- function(gr, id.field=0, measure.field=NULL) {
+  ## Include seqnames into the unique ID
+  unique.ID <- paste0(seqnames(gr), '_', mcols(gr)[,id.field])
+  ## Get continous runs of the same unique ID
+  unique.ID.runLength <- runLength(Rle(unique.ID))
+  ind.last <- cumsum(unique.ID.runLength) ##get indices of last range in a consecutive(RLE) run of the same value
+  ind.first <- c(1,cumsum(unique.ID.runLength) + 1) ##get indices of first range in a consecutive(RLE) run of the same value
+  ind.first <- ind.first[-length(ind.first)]  ##erase last index from first range indices 
   collapsed.gr <- GenomicRanges::GRanges(seqnames=seqnames(gr[ind.first]), ranges=IRanges(start=start(gr[ind.first]), end=end(gr[ind.last])), mcols=mcols(gr[ind.first]))
   names(mcols(collapsed.gr)) <- names(mcols(gr[ind.first]))
+  ## Sum values in user defined measure fields(s)
+  if (!is.null(measure.field)) {
+    run.ID  <- factor(rep(seq_along(unique.ID.runLength), unique.ID.runLength))
+    for (field in measure.field) {
+      mcols(collapsed.gr)[,field] <-  tapply(mcols(gr)[,field], run.ID, sum)
+      #collapsed.gr$C <-  tapply(gr$C, run.ID, sum)
+      #collapsed.gr$W <-  tapply(gr$W, run.ID, sum)
+    }
+  }
   return(collapsed.gr)
 }
 
@@ -224,13 +237,17 @@ reformat <- function(v) {
 #' Expand Genomic Ranges into a set of region boundaries
 #' 
 #' @param gr A \code{\link{GRanges}} object.
-#' 
 #' @author David Porubsky
 #' @export
 #'
 getRegionBoundaries <- function(gr) {
+  ## Extract start and end position of each genomic range
   starts.gr <- GRanges(seqnames = seqnames(gr), ranges = IRanges(start=start(gr), end=start(gr)))
   ends.gr <- GRanges(seqnames = seqnames(gr), ranges = IRanges(start=end(gr), end=end(gr)))
-  all.pos.gr <- sort(c(starts.gr, ends.gr))
+  ## Region ID to both start and end position
+  starts.gr$region.ID <- as.character(gr)
+  ends.gr$region.ID <- as.character(gr)
+  ## Sort and export region boundaries
+  all.pos.gr <- sort(c(starts.gr, ends.gr), ignore.strand=TRUE)
   return(all.pos.gr)
 }
